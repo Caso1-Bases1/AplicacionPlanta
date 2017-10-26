b628c3e3891a1ddb98e90ac47991745cb12ebb28
nlrarojas <nlrarojas@hotmail.com>
2017-10-25 21:07:24 -0600

    Menu implementation

--- src/view/PrincipalWindow.java
+++ src/view/PrincipalWindow.java
@@ -66,13 +66,73 @@ public class PrincipalWindow extends JFrame {
 		
 		JMenuItem mntmEliminarEmpleado = new JMenuItem("Eliminar Empleado");
 		mnEmpleados.add(mntmEliminarEmpleado);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JButton btnAbrirMarcas = new JButton("Abrir Marcas");
-		btnAbrirMarcas.addActionListener(new ActionListener() {
+		JMenu mnTiposDeEmpleados = new JMenu("Tipos de empleados");
+		mnMantenimiento.add(mnTiposDeEmpleados);
+		
+		JMenuItem mntmNuevoTipoDe = new JMenuItem("Nuevo tipo de empleado");
+		mnTiposDeEmpleados.add(mntmNuevoTipoDe);
+		
+		JMenuItem mntmBuscarTipoDe = new JMenuItem("Buscar tipo de empleado");
+		mnTiposDeEmpleados.add(mntmBuscarTipoDe);
+		
+		JMenuItem mntmModificarTipoDe = new JMenuItem("Modificar tipo de empleado");
+		mnTiposDeEmpleados.add(mntmModificarTipoDe);
+		
+		JMenuItem mntmEliminarTipoDe = new JMenuItem("Eliminar tipo de empleado");
+		mnTiposDeEmpleados.add(mntmEliminarTipoDe);
+		
+		JMenu mnDasLaborables = new JMenu("D\u00EDas laborables");
+		mnMantenimiento.add(mnDasLaborables);
+		
+		JMenu mnAdministrarFeriados = new JMenu("Administrar feriados");
+		mnDasLaborables.add(mnAdministrarFeriados);
+		
+		JMenuItem mntmNuevoDaLaborable = new JMenuItem("Nuevo d\u00EDa laborable");
+		mnAdministrarFeriados.add(mntmNuevoDaLaborable);
+		
+		JMenuItem mntmBuscarDaLaborable = new JMenuItem("Buscar d\u00EDa laborable");
+		mnAdministrarFeriados.add(mntmBuscarDaLaborable);
+		
+		JMenuItem mntmModificarDaLaborable = new JMenuItem("Modificar d\u00EDa laborable");
+		mnAdministrarFeriados.add(mntmModificarDaLaborable);
+		
+		JMenuItem mntmEliminarDaLaborable = new JMenuItem("Eliminar d\u00EDa laborable");
+		mnAdministrarFeriados.add(mntmEliminarDaLaborable);
+		
+		JMenu mnAdministrarDiasLaborables = new JMenu("Administrar dias laborables");
+		mnDasLaborables.add(mnAdministrarDiasLaborables);
+		
+		JMenuItem mntmNuevoFeriado = new JMenuItem("Nuevo feriado");
+		mnAdministrarDiasLaborables.add(mntmNuevoFeriado);
+		
+		JMenuItem mntmBuscarFeriado = new JMenuItem("Buscar feriado");
+		mnAdministrarDiasLaborables.add(mntmBuscarFeriado);
+		
+		JMenuItem mntmModificarFeriado = new JMenuItem("Modificar feriado");
+		mnAdministrarDiasLaborables.add(mntmModificarFeriado);
+		
+		JMenuItem mntmEliminarFeriado = new JMenuItem("Eliminar feriado");
+		mnAdministrarDiasLaborables.add(mntmEliminarFeriado);
+		
+		JMenu mnCdigoDePlanta = new JMenu("C\u00F3digo de planta");
+		mnMantenimiento.add(mnCdigoDePlanta);
+		
+		JMenuItem mntmModificarCdigoDe = new JMenuItem("Modificar c\u00F3digo de planta");
+		mntmModificarCdigoDe.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				UpdatePlantCodeView upc = new UpdatePlantCodeView();
+				contentPane.add(upc).setBounds(0, 0, 800, 600);
+				repaint();
+			}
+		});
+		mnCdigoDePlanta.add(mntmModificarCdigoDe);
+		
+		JMenu btnMarcas = new JMenu("Marcas");
+		menuBar.add(btnMarcas);
+		
+		JMenuItem mntmAbrirMdulo = new JMenuItem("Abrir m\u00F3dulo");
+		mntmAbrirMdulo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (!Utility.IS_MARCAS_OPEN){
 					AplicacionMarcas window = new AplicacionMarcas();
@@ -80,6 +140,10 @@ public class PrincipalWindow extends JFrame {
 				}
 			}
 		});
-		contentPane.add(btnAbrirMarcas);
+		btnMarcas.add(mntmAbrirMdulo);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 	}
 }
---
+++ src/view/UpdatePlantCodeView.java
@@ -0,0 +1,48 @@
+package view;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class UpdatePlantCodeView extends JPanel {
+	private JTextField textField_1;
+
+	/**
+	 * Create the panel.
+	 */
+	public UpdatePlantCodeView() {
+		setLayout(null);
+		
+		JLabel lblMidificarCdigoDe = new JLabel("Midificar c\u00F3digo de planta");		
+		lblMidificarCdigoDe.setBounds(159, 33, 212, 14);
+		add(lblMidificarCdigoDe);
+		
+		JLabel lblCdigoDePlanta = new JLabel("C\u00F3digo de planta");
+		lblCdigoDePlanta.setBounds(66, 122, 140, 14);
+		add(lblCdigoDePlanta);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(216, 119, 155, 20);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnModificar = new JButton("Modificar");
+		btnModificar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					int codePlant = Integer.parseInt(textField_1.getText());
+				} catch (NumberFormatException ex){
+					JOptionPane.showMessageDialog(null, "Código incorrecto");
+				}
+			}
+		});
+		btnModificar.setBounds(173, 184, 89, 23);
+		add(btnModificar);		
+	}
+}
